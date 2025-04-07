c53f6d246bb7edb1ff67394cd088e041c7ee7352
Yukari Chiba <i@0x7f.cc>
2025-04-07 13:45:21 +0800

    update about page

--- components/MainAbout.vue
+++ components/MainAbout.vue
@@ -8,7 +8,7 @@
         </div>
         <v-card-title class="mb-4">
             <v-icon large start>mdi-pound</v-icon>
-            About
+            About / 关于
         </v-card-title>
         <v-card-text>
             <h2>English Version</h2>
@@ -24,21 +24,36 @@
                 All logs will be uploaded to LOTW, HRDLOG and QRZ in 72 hrs. If
                 your record has not been uploaded, please contact me.
             </p>
+            <p>
+                Log will first be uploaded to the LOTW. HRDLOG/ham365 will be
+                updated automatically from the LOTW and QRZ will be updated
+                manually from the LOTW.
+            </p>
+            <p>
+                Platforms other than LOTW / HRDLOG / ham365 / QRZ are used
+                infrequently and there is no guarantee that logs will be
+                updated.
+            </p>
             <h3>QSL Card Exchange</h3>
             <p>QSL card exchange is welcomed.</p>
             <p>
                 All cards received will be replied to, SASE is not required, it
                 is recommended to provide your address in the mail.
             </p>
-            <v-alert
-                density="compact"
-                title="QSL Card Postal Address (Public, Receive):"
-            >
-                YUKARI<br />
-                P.O. Box 399-5, HuiNan Post Office<br />
-                Shanghai, P.R.China 201399<br />
-                +86-21-58223996
+            <v-alert density="compact">
+                <code>
+                    YUKARI<br />
+                    P.O. Box 399-5, HuiNan Post Office<br />
+                    Shanghai, P.R.China 201399<br />
+                    +86-21-58223996
+                </code>
             </v-alert>
+            <p>
+                Please note that the public receiving address here may be
+                different from the address we provide when we send out
+                unsolicited QSL cards. If you have received a QSL card from us,
+                please return it to the address provided in the envelope.
+            </p>
         </v-card-text>
         <v-divider />
         <v-card-text>
@@ -53,18 +68,33 @@
                 所有通联日志都将在 72 小时内被上传至 LOTW, HRDLOG 和
                 QRZ，如果您的记录未被上传，请与我联系。
             </p>
+            <p>
+                通联日志首先会上传至 LOTW，HRDLOG/ham365 将自动从 LOTW 更新，QRZ
+                将从 LOTW 手动更新。
+            </p>
+            <p>
+                LOTW / HRDLOG/ham365 / QRZ
+                以外的平台不经常使用，不能保证日志会更新。
+            </p>
             <h3>QSL 卡片交换</h3>
             <p>欢迎进行 QSL 卡片的交换。</p>
             <p>
                 收到的所有卡片都会回复，SASE
                 不是必需，但是建议信件内附您的地址。
             </p>
-            <v-alert density="compact" title="QSL 卡片邮寄地址（公开，接收）:">
-                YUKARI（收）<br />
-                上海市 399-5 邮政信箱<br />
-                邮编：201399<br />
-                021-58223996
+            <v-alert density="compact">
+                <code>
+                    YUKARI（收）<br />
+                    上海市 399-5 邮政信箱<br />
+                    邮编：201399<br />
+                    021-58223996
+                </code>
             </v-alert>
+            <p>
+                请注意，此处的公开接收地址可能与本台主动发送 QSL
+                卡片时提供的接收地址不同。如果您收到了本台的 QSL
+                卡片，请按信封内提供的地址寄回即可。
+            </p>
         </v-card-text>
     </div>
 </template>

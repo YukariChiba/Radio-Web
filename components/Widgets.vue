e2da57106a2cb9203946866afabcf6bf430d8b81
Yukari Chiba <i@0x7f.cc>
2025-05-19 18:20:37 +0800

    widgets: adapt for dark mode

--- components/Widgets.vue
+++ components/Widgets.vue
@@ -6,10 +6,32 @@
         <v-img
             src="https://hamalert.org/myspot?c=BH8AEA&h=dc7ee3562aa2a07e&hr=1"
             max-width="300px"
+            class="img-light"
         />
         <v-img
             src="https://www.hamqsl.com/solar2.php?bgcolor=white&back=transparent"
             max-width="300px"
+            class="img-light"
+        />
+        <v-img
+            src="https://hamalert.org/myspot?c=BH8AEA&h=dc7ee3562aa2a07e&hr=1&dark=1"
+            max-width="300px"
+            class="img-dark"
+        />
+        <v-img
+            src="https://www.hamqsl.com/solar2.php?bgcolor=black&back=transparent"
+            max-width="300px"
+            class="img-dark"
         />
     </v-card>
 </template>
+
+<style scoped>
+.v-theme--light .img-dark {
+    display: none;
+}
+
+.v-theme--dark .img-light {
+    display: none;
+}
+</style>
